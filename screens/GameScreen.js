d5acf83638c44430d696ee443647aee62138b695
Arnav Choudhury <arnav13@gmail.com>
2020-01-22 19:01:57 +0530

    Added Flatist view to show and render all of the guesses made so far, including the current guess.

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,6 @@
 import React, {useState, useRef, useEffect} from 'react';
-import { View, Text, StyleSheet, Button, Alert } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
 
 
 import MainButton from "../components/MainButton";
@@ -23,46 +22,76 @@ const generateRandomNumber = (min, max, exclude) => {
 
 
 const GameScreen = (props) => {
-	const [currentGuess, currentGuessUpdate] = useState(generateRandomNumber(1, 100, props.userChoice));
+	const initialGuess = generateRandomNumber(1, 100, props.userChoice);
+	const [currentGuess, currentGuessUpdate] = useState(initialGuess);
+	const [pastGuesses, pastGuessesUpdate] = useState([
+		{
+			key: initialGuess.toString(),
+			value: initialGuess
+		}
+		]
+	);
+
 	const currentLow = useRef(1);
 	const currentHigh = useRef(100);
 	let numOfGuesses = useRef(0);
 
 
+	const updatePastGuessesHandler = (guess) => {
+		console.log("Current guess is " + guess);
+		pastGuessesUpdate([
+			...pastGuesses, {
+				key: guess.toString(), value: guess
+			}
+		]);
+	};
+
 	const updateGuessGreater = () => {
 
-		if ( currentGuess > props.userChoice ) {
+		if (currentGuess > props.userChoice) {
 
 			Alert.alert("Don\'t lie!", "You know this is not correct. Press Lower!", [{text: "Okay", style: "cancel"}]);
 			return
 		}
 
-		currentLow.current = currentGuess;
+		currentLow.current = currentGuess + 1;
 		const nextNumber = generateRandomNumber(currentLow.current, currentHigh.current, currentGuess);
 		currentGuessUpdate(nextNumber);
+
+		// Update the Past Guesses Array
+		updatePastGuessesHandler(nextNumber);
+
 	};
 	const updateGuessLower = () => {
 
-		if ( currentGuess < props.userChoice ) {
+		if (currentGuess < props.userChoice) {
 
-			Alert.alert("Don\'t lie!", "You know this is not correct. Press Greater!", [{text: "Okay", style: "cancel"}]);
+			Alert.alert("Don\'t lie!", "You know this is not correct. Press Greater!", [{
+				text: "Okay",
+				style: "cancel"
+			}]);
 			return
 		}
 
 		currentHigh.current = currentGuess;
 		const nextNumber = generateRandomNumber(currentLow.current, currentHigh.current, currentGuess);
 		currentGuessUpdate(nextNumber);
+
+		// Update the Past Guesses Array
+		updatePastGuessesHandler(nextNumber);
+
 	};
 
-	useEffect( () => {
+	useEffect(() => {
 
 		numOfGuesses.current += 1;
 
-		if ( currentGuess === props.userChoice ) {
+		if (currentGuess === props.userChoice) {
 			props.onGameOver(numOfGuesses.current);
 		}
 
 	});
+
 
 	return (
 		<View style={styles.screen}>
@@ -75,10 +104,25 @@ const GameScreen = (props) => {
 				<MainButton onPress={updateGuessGreater}>
 					<Ionicons name="md-add" size={24} color="white"/>
 				</MainButton>
-				<MainButton  onPress={updateGuessLower}>
+				<MainButton onPress={updateGuessLower}>
 					<Ionicons name="md-remove" size={24} color="white"/>
 				</MainButton>
 			</Card>
+			<Text style={Defaultstyles.titleText}> Guesses so far: </Text>
+			<FlatList
+				data={pastGuesses}
+				ListEmptyComponent={<View><Text>NO ITEMS</Text></View>} //TODO Needs to be styles in a better way. Show some image instead.
+				renderItem={
+					item => {
+						console.log("item is ", item);
+						return (
+							<View>
+								<Text> {item.item.value} </Text>
+							</View>
+						);
+					}
+				}
+			/>
 
 		</View>
 	);
